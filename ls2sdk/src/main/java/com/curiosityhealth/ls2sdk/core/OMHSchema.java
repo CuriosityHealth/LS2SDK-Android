1f802f757b3ad02b1f58d9fe9aeac36416d7e89b
jdkizer9 <jdkizer9@users.noreply.github.com>
2018-03-13 20:27:38 -0400

    Refactored project a bit

---
+++ ls2sdk/src/main/java/com/curiosityhealth/ls2sdk/core/OMHSchema.java
@@ -0,0 +1,29 @@
+package com.curiosityhealth.ls2sdk.core;
+
+/**
+ * Created by jameskizer on 2/2/17.
+ */
+public class OMHSchema {
+    private String name;
+    private String namespace;
+    private String version;
+
+    public OMHSchema(String name, String namespace, String version) {
+        this.name = name;
+        this.namespace = namespace;
+        this.version = version;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+}
